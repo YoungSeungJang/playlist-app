e0440281662faf6b33d5638f2524c56a1f1dff7e
YoungSeungJang <1345620@naver.com>
2025-09-08 19:49:30 +0900

    :hammer: 로그인 모달, 회원가입 모달 추가

--- apps/web/src/App.tsx
+++ apps/web/src/App.tsx
@@ -1,10 +1,14 @@
 import Layout from './components/layout/Layout'
 import AppRoutes from './routes'
+import LoginModal from './components/auth/LoginModal'
+import RegisterModal from './components/auth/RegisterModal'
 
 function App() {
   return (
     <Layout>
       <AppRoutes />
+      <LoginModal />
+      <RegisterModal />
     </Layout>
   )
 }
